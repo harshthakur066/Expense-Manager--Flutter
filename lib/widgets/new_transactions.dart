7428250775d55e09c76f69cf64bc52ec3d002d6d
harshthakur066 <harshthakur066@gmail.com>
2020-05-25 19:36:01 +0530

    improved user interface

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -7,6 +7,20 @@ class NewTransations extends StatelessWidget {
 
   NewTransations(this.addTransaction);
 
+  void submitData() {
+    final enterdTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enterdTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTransaction(
+      enterdTitle,
+      enteredAmount,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -18,20 +32,18 @@ class NewTransations extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Tiltle'),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
             ),
             TextField(
-              decoration: InputDecoration(labelText: 'Tiltle'),
+              decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => submitData(),
             ),
             FlatButton(
               child: Text('Add Transaction'),
               textColor: Colors.purple,
-              onPressed: () {
-                addTransaction(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
+              onPressed: submitData,
             ),
           ],
         ),
